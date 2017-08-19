df34a2856791c1a158efc3451de869e855a015c3
ywxf <997720736@qq.com>
2017-08-19 17:44:00 +0800

    第二个包

---
+++ src/com/zhiyou100/cs02/c.java
@@ -0,0 +1,12 @@
+package com.zhiyou100.cs02;
+
+public class c {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int aa = 2;
+		System.out.println(aa);
+	}
+
+}
